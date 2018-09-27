688d7a15342291e00219b4fb4e31840e33724316
Carlo Bertini <waydotnet@gmail.com>
2018-09-27 14:26:28 +0200

    first commit

---
+++ pacakges.el
@@ -0,0 +1,14 @@
+;;; ~/.doom.d/pacakges.el -*- lexical-binding: t; -*-
+
+;;;  -*- lexical-binding: t; -*-
+;;;  -*- no-byte-compile: t; -*-
+
+;(package! parinfer)
+
+(package! indium)
+(package! reason-mode)
+(package! prettier-js)
+(package! org-fancy-priorities)
+(package! move-text)
+(package! vue-mode)
+(package! origami)
